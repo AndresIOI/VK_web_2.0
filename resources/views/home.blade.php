409631801401c516649e9bf3d4176338c30eed06
Daniel.Cruz <danielcruz1im18@gmail.com>
2018-11-11 07:52:32 -0600

    Añadidos los estilos de subir video y subir pdf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
     <a class="nav-link" name="subircontenido_tab" data-toggle="tab" href="#subircontenido_tab" role="tab" aria-controls="contact" aria-selected="false">Subir Videos</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" name="subircontenido_tab_pdf" data-toggle="tab" href="#subircontenido_tab_pdf" role="tab" aria-controls="contact" aria-selected="false">Subir Pdf</a>
+    <a class="nav-link" name="subircontenido_tab_pdf" data-toggle="tab" href="#subircontenido_tab_pdf" role="tab" aria-controls="contact" aria-selected="false">Subir PDF</a>
   </li>
   @if(Auth::User()->rol_id == 2)
   <li class="nav-item">
@@ -27,73 +27,110 @@
   @endif
 </ul>
 <div class="tab-content" id="myTabContent">
-  <div class="tab-pane fade show active" id="examen_tab" role="tabpanel" aria-labelledby="home-tab">Aquí irá la sección de examenes</div>
-  <div class="tab-pane fade" id="estadisticas_tab" role="tabpanel" aria-labelledby="profile-tab">Aquí irá la sección de estadisticas</div>
-  <div class="tab-pane fade" id="videos_tab" role="tabpanel" aria-labelledby="contact-tab"> @yield('content2')</div>
-  <div class="tab-pane fade" id="pdf_tab" role="tabpanel" aria-labelledby="contact-tab">
-  <div class="card-deck">
-  @foreach($pdfs as $pdf)
-  <div class="card">
-    <img class="card-img-top" src=".../100px200/" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+  <div class="tab-pane fade show active" id="examen_tab" role="tabpanel" aria-labelledby="home-tab">
+
+  </br>
+  </br>
+    <div class="form-group" align="center">
+
+      <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src=".../100px180/" alt="Card image cap" width="150px" height="150px">
+        <div class="card-body">
+          <h5 class="card-title">Física para Informáticos</h5>
+          <p class="card-text">Examen parcial de la materia de Física para Informáticos</p>
+          <a href="#" class="btn btn-primary">Hacer Prueba</a>
+        </div>
+      </div>
+
     </div>
+
+
+
+
   </div>
-    
-  @endforeach  
-  </div>
+
+  <div class="tab-pane fade" id="estadisticas_tab" role="tabpanel" aria-labelledby="profile-tab">Aquí irá la sección de estadisticas</div>
+  <div class="tab-pane fade" id="videos_tab" role="tabpanel" aria-labelledby="contact-tab"> Videos </div>
+  <div class="tab-pane fade" id="pdf_tab" role="tabpanel" aria-labelledby="contact-tab">
+
+      <div class="card-deck">
+        @foreach($pdfs as $pdf)
+        <div class="card">
+          <img class="card-img-top" src=".../100px200/" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+
+        @endforeach
+      </div>
+
   </div>
   <div class="tab-pane fade"  id="subircontenido_tab" role="tabpanel" aria-labelledby="contact-tab">
-  
-  <h1>Formulario Subir Video </h1>
-    <form method="post" action="{{route('video.store')}}" enctype ="multipart/form-data">
-    @csrf
-    <label> Titulo del video</label>
-    <input type="text" name = "titulo_video">
-    <br>
-    <br>
-    <label >Video</label>
-    <input type="file" name="video" >
-    <br>
-    <br>
-    <select name="etiqueta">
-        <option selected ="selected">Seleccione la etiqueta</option>
-        @foreach($etiquetas as $etiqueta )
-        <option value="{{$etiqueta['id_etiqueta']}}">{{$etiqueta['materia']}}</option>
-        @endforeach
-    </select>
-    <br>
-    <br>
-    <button type="submit">agregar video</button>
-    </form>
-  
+</br>
+    <div class="container" align="center">
+
+        <form method="post" action="{{route('video.store')}}" enctype ="multipart/form-data" class="form-group">
+          <div class="card border-dark mb-3" style="max-width: 25rem;" align="center">
+          <div class="card-header">Subir video</div>
+          <div class="card-body text-dark">
+            @csrf
+            <input type="text" name = "titulo_video" placeholder="Titulo" class="form-control">
+            <br>
+            <div class="custom-file">
+              <label class="custom-file-label" for="customFileLang" align="left">Video</label>
+              <input type="file" name="video" class="custom-file-input" lang="es" >
+            </div>
+            <br>
+            <br>
+            <select name="etiqueta" class="form-control">
+                <option selected ="selected">Seleccione la etiqueta</option>
+                @foreach($etiquetas as $etiqueta )
+                <option value="{{$etiqueta['id_etiqueta']}}">{{$etiqueta['materia']}}</option>
+                @endforeach
+            </select>
+            <br>
+            <br>
+            <button type="submit" class="btn btn-primary">Agregar Video</button>
+          </div>
+          </div>
+        </form>
+
+    </div>
+
   </div>
   <div class="tab-pane fade"  id="subircontenido_tab_pdf" role="tabpanel" aria-labelledby="contact-tab">
-  
-  <h1>Formulario Subir Pdf </h1>
-    <form method="post" action="{{route('pdf.store')}}" enctype ="multipart/form-data">
-    @csrf
-    <label> Titulo del pdf</label>
-    <input type="text" name = "titulo_pdf">
-    <br>
-    <br>
-    <label >pdf</label>
-    <input type="file" name="pdf" >
-    <br>
-    <br>
-    <select name="etiqueta">
-        <option selected ="selected">Seleccione la etiqueta</option>
-        @foreach($etiquetas as $etiqueta )
-        <option value="{{$etiqueta['id_etiqueta']}}">{{$etiqueta['materia']}}</option>
-        @endforeach
-    </select>
-    <br>
-    <br>
-    <button type="submit">agregar pdf</button>
-    </form>
-  
+    </br>
+      <div class="container" align="center">
+          <form method="post" action="{{route('pdf.store')}}" enctype ="multipart/form-data" class="form-group">
+            <div class="card border-dark mb-3" style="max-width: 25rem;" align="center">
+              <div class="card-header">Subir PDF</div>
+              <div class="card-body text-dark">
+                @csrf
+                <input type="text" name = "titulo_pdf" class="form-control" placeholder="Titulo">
+                <br>
+                <div class="custom-file">
+                  <label class="custom-file-label" for="customFileLang" align="left">PDF</label>
+                  <input type="file" name="pdf" class="custom-file-input" lang="es" >
+                </div>
+                <br>
+                <br>
+                <select name="etiqueta" class="form-control">
+                    <option selected ="selected">Seleccione la etiqueta</option>
+                    @foreach($etiquetas as $etiqueta )
+                    <option value="{{$etiqueta['id_etiqueta']}}">{{$etiqueta['materia']}}</option>
+                    @endforeach
+                </select>
+                <br>
+                <br>
+                <button type="submit" class="btn btn-primary">Agregar PDF</button>
+              </div>
+            </div>
+          </form>
+      </div>
   </div>
   </div>
   @if(Auth::User()->rol_id == 1)
